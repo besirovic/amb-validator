01d538a0ebc00f7a1e5e1128b253f9b628ef4dad
Ademir Besirovic <ademir.besirovic1996@gmail.com>
2018-09-06 14:36:12 +0200

    Make header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import AMBLogo from '../../assets/logo.png';
+
+import './header.css';
+
+const Header = () => (
+  <div className="header">
+    <img src={AMBLogo} alt="Ambrosus logo" />
+  </div>
+);
+
+export default Header;
