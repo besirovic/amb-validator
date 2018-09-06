0e459b15ff9479a23a0bd8ac887673857a40ffaa
Ademir Besirovic <ademir.besirovic1996@gmail.com>
2018-09-06 14:37:59 +0200

    Add material components

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+
 import registerServiceWorker from './registerServiceWorker';
+
+import 'typeface-roboto';
+import 'normalize.css';
+import '@mdi/font/css/materialdesignicons.css';
+import '@material/card/dist/mdc.card.css';
+import '@material/button/dist/mdc.button.css';
+import '@material/fab/dist/mdc.fab.css';
+import '@material/dialog/dist/mdc.dialog.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
